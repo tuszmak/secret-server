b89dd1ebedcbeffcd3eb4b10d6c29a65ae3a106b
tuszmak <daniel.pinter02@edu.bme.hu>
2023-09-28 10:20:37 +0200

    feat: init base of project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <title>Secret server</title>
+      </Head>
+      <div>
+        
+      </div>
+    </main>
+  );
+}
