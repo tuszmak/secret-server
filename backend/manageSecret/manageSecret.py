a95e2384549c846ce3996be99dd0e23e1adca8e1
tuszmak <daniel.pinter02@edu.bme.hu>
2023-11-24 17:06:44 +0100

    refactor: change param type to work with prev commit

--- backend/manageSecret/manageSecret.py
+++ backend/manageSecret/manageSecret.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append('../')
-from model.secretData import secretData
+from model.secretData import SecretData
+from db.createSecret import *
 
-def createSecret(data : secretData):
+def createSecret(data : SecretData):
     if(data.text != None & data.numberOfVisits != 0 & data.expDate!=None):
         print("Fetch is cool!")
         print(data.expDate.minute)
