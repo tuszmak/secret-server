5dee12bcf25264ab6c03bc3b2525b0e7dcdf0130
tuszmak <daniel.pinter02@edu.bme.hu>
2023-11-28 21:20:30 +0100

    style: add a comment

--- backend/db/getConn.py
+++ backend/db/getConn.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+# This function only exists to reduce code duplication.
 def getConn():
     conn = psycopg2.connect(
               host="localhost",
@@ -7,5 +8,4 @@ def getConn():
               user=os.environ['DB_USERNAME'],
               password=os.environ['DB_PASSWORD'])
       
-      # Open a cursor to perform database operations
     return conn
