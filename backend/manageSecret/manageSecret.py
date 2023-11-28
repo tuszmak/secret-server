171fdc7ad7211497d2bbb3287ff98f773b7b19eb
tuszmak <daniel.pinter02@edu.bme.hu>
2023-11-28 22:16:28 +0100

    fix: make function raise exception instead of print

--- backend/manageSecret/manageSecret.py
+++ backend/manageSecret/manageSecret.py
@@ -7,4 +7,5 @@ from db.createSecret import createSecret
 def createSecretDAO(data : SecretData):
     if(data.text != "" and data.numberOfVisits != 0 and data.expDate!=None):
         createSecret(data)
-    else: print("Something is missing :c")
+    else: 
+        raise Exception("Something is missing :c")
