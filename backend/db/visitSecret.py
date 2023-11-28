28d683a66237a9075bede5e69fb7ed886c5f63ac
tuszmak <daniel.pinter02@edu.bme.hu>
2023-11-28 17:24:23 +0100

    feat: format return data from getSecretFromDb

--- backend/db/visitSecret.py
+++ backend/db/visitSecret.py
@@ -9,16 +9,20 @@ def getSecretFromDb(hash : str):
     secret = []
     print(hash)
     getQuery = "SELECT secret FROM secrets WHERE link = %s;"
-    didWork = False
     try:
-              cur.execute(getQuery, (hash,))
+              cur.execute(getQuery, (hash,)) # The execute wants a tuple as parameter, that's the weird parameters.
               secret = cur.fetchall()
-              didWork= True
     except Exception: 
               print(traceback.print_exc())
-              didWork= False
-              print("Something's wrong with getting stuff")
+              raise Exception("Select query can't be executed")
+              
       
     cur.close()
-    print(secret[0][0])
-    return secret
+    
+    if(len(secret) == 0):
+      return None
+    elif(len(secret) > 1):
+      raise Exception("WTF there are two secrets with the same hash.")
+    else:
+      return secret[0][0]
+            
