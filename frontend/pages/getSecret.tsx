0ef377d009d736456bf79e5bc6636fe92175f14b
tuszmak <daniel.pinter02@edu.bme.hu>
2023-12-05 09:31:49 +0100

    feat: show secret on frontend

--- frontend/pages/getSecret.tsx
+++ frontend/pages/getSecret.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function getSecret() {
   const [hash, setHash] = useState<string>("");
+  const [secretString, setSecretString] = useState<string>("");
   const getSecret = async () => {
     const request: SecretFetchRequest = {
       hash: hash,
@@ -14,10 +16,22 @@ export default function getSecret() {
       },
     });
     const secret: Secret = await response.json();
+    setSecretString(secret.secret);
   };
-
-  return (
+  return secretString ? (
     <div>
+      <button>
+        <Link href="/">Back</Link>
+      </button>
+      <div>
+        Your secret is: {secretString}
+      </div>
+    </div>
+  ) : (
+    <div>
+      <button>
+        <Link href="/">Back</Link>
+      </button>
       <label htmlFor="hash">Input hash</label>
       <input
         type="text"
