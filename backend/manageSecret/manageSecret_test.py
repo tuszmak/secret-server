355eb396b4f3c14886ce36e62febcbe04ff39eb6
tuszmak <daniel.pinter02@edu.bme.hu>
2023-12-11 12:52:01 +0100

    refactor: migrate tests to pytest

--- backend/manageSecret/manageSecret_test.py
+++ backend/manageSecret/manageSecret_test.py
@@ -1,26 +1,26 @@
-import sys
-import unittest
 from model.secretData import SecretData
 from datetime import datetime
-from unittest.mock import Mock, patch, MagicMock
-from db.createSecret import createSecret
+import pytest
+from db import createSecret
 from manageSecret.manageSecret import createSecretDAO
-class ManageSecret_test(unittest.TestCase):
+from unittest.mock import patch, Mock
+
+class TestManageSecret():
     def test_create_secret_without_text(self):
         foo = SecretData("", 5, datetime.now())
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             createSecretDAO(foo)
     def test_create_secret_without_zero_visits(self):
         foo = SecretData("foo", 0, datetime.now())
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             createSecretDAO(foo)
     def test_create_secret_without_exp_date(self):
         foo = SecretData("foo", 5, None)
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             createSecretDAO(foo)
     def test_create_secret_with_no_data(self):
         foo = SecretData("", 0, None)
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             createSecretDAO(foo)
     @patch(target="manageSecret.manageSecret.createSecret")
     def test_creating_data(self, mock_createSecret : Mock):
