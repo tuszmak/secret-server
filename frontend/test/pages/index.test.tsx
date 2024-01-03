7e7daa691968c3ec90e465df4a2dc4ab17a626f1
Tuszmak <daniel.pinter02@gmail.com>
2024-01-03 09:58:54 +0100

    style: remove behavior describe

--- frontend/test/pages/index.test.tsx
+++ frontend/test/pages/index.test.tsx
@@ -3,35 +3,34 @@ import "@testing-library/jest-dom";
 import { userEvent } from "@testing-library/user-event";
 import Home from "@/pages";
 import { describe } from "node:test";
+import React, { ReactElement } from "react";
 
-describe("Render", ()=>{
-    it("Should have a heading", ()=>{
-        render(<Home />)
-        const title = screen.getByRole("heading")
-        expect(title).toBeInTheDocument()
-    })
-    it("Should have a heading with 'Hello there in it'", ()=>{
-        render(<Home />)
-        const title = screen.getByText("Hello there")
-        expect(title).toBeInTheDocument()
-    })
-    //This test is temporary, as is the sample text.
-    it("Should have a paragraph with sample text in it", ()=>{
-        render(<Home />)
-        const paragraph = screen.getByText(/Provident/i)
-        expect(paragraph).toBeInTheDocument()
-    })
-    it("Should have a button, with create secret text", ()=>{
-        render(<Home />)
-        const createButton = screen.getByText("Create secret")
-        expect(createButton).toBeInTheDocument()
-    })
-    it('Should have a button, with "view secret" text', ()=>{
-        render(<Home />)
-        const viewButton = screen.getByText("View secret")
-        expect(viewButton).toBeInTheDocument()
-    })
-})
-describe("Behavior", ()=>{
-    
-})
+describe("Render", () => {
+  it("Should have a heading", () => {
+    render(<Home />);
+    const title = screen.getByRole("heading");
+    expect(title).toBeInTheDocument();
+  });
+  it("Should have a heading with 'Hello there in it'", () => {
+    render(<Home />);
+    const title = screen.getByText("Hello there");
+    expect(title).toBeInTheDocument();
+  });
+  //This test is temporary, as is the sample text.
+  it("Should have a paragraph with sample text in it", () => {
+    render(<Home />);
+    const paragraph = screen.getByText(/Provident/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+  it("Should have a button, with create secret text", () => {
+    render(<Home />);
+    const createButton = screen.getByText("Create secret");
+    expect(createButton).toBeInTheDocument();
+  });
+  it('Should have a button, with "view secret" text', () => {
+    render(<Home />);
+    const viewButton = screen.getByText("View secret");
+    expect(viewButton).toBeInTheDocument();
+  });
+});
+
