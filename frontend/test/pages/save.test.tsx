945e159e81127c43364404c8a7a54097e8876bb4
Tuszmak <daniel.pinter02@gmail.com>
2024-01-04 13:06:32 +0100

    refactor: put renders in a beforeeach

--- frontend/test/pages/save.test.tsx
+++ frontend/test/pages/save.test.tsx
@@ -1,18 +1,27 @@
 import Save from "@/pages/save";
-import { getByRole, getByTestId, render, screen } from "@testing-library/react";
+import {
+  getByRole,
+  getByTestId,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { userEvent } from "@testing-library/user-event";
 import Home from "@/pages";
 import { describe } from "node:test";
+
+beforeEach(() => {
+  render(<Save />);
+});
+
 describe("State when there's no link", () => {
   describe("Render", () => {
     it("Should render the back button", () => {
-      render(<Save />);
       const backButton = screen.getByText("Back");
       expect(backButton).toBeInTheDocument();
     });
     it("Should render the input fields", () => {
-      render(<Save />);
       const secretInput = screen.getByText("What is your secret?");
       const visitInput = screen.getByText("How many visits do you allow?");
       const dateInput = screen.getByText("When should it expire naturally?");
@@ -21,7 +30,6 @@ describe("State when there's no link", () => {
       expect(dateInput).toBeInTheDocument();
     });
     it("Should render the submit button", () => {
-      render(<Save />);
       const submitButton = screen.getByText("Submit");
       expect(submitButton).toBeInTheDocument();
     });
@@ -30,11 +38,10 @@ describe("State when there's no link", () => {
   it("Should call handleSubmit when the Submit button is clicked", async () => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        json: () => Promise.resolve({ test: 100 }),
+        json: () => Promise.resolve({ text: "foo" }),
       })
     ) as jest.Mock;
 
-    render(<Save />);
     const submitButton = screen.getByTestId("submitButton");
     await userEvent.click(submitButton);
     expect(global.fetch).toHaveBeenCalled();
