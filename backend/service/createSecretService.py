5227145396005f4e9da8b8cc1bdee2f1f8a899ce
Tuszmak <daniel.pinter02@gmail.com>
2024-01-22 12:43:00 +0100

    refactor: rename method and variable names to follow PEP

--- backend/service/createSecretService.py
+++ backend/service/createSecretService.py
@@ -2,10 +2,10 @@ from model import SecretData
 from db import createSecret
 from datetime import datetime
 
-def createSecretDAO(data : dict, envVariables):
-    parsedDate = datetime.fromisoformat(str(data.get("expiryDate")))
-    newSecretData = SecretData(data.get("secret"),data.get("numberOfVisits"), parsedDate)
-    if(newSecretData.text != "" and newSecretData.numberOfVisits != 0 and newSecretData.expDate!=None):
-        return createSecret(data, envVariables)
+def create_secret_dao(data : dict, env_variables):
+    parsed_date = datetime.fromisoformat(str(data.get("expiryDate")))
+    new_secret_data = SecretData(data.get("secret"),data.get("numberOfVisits"), parsed_date)
+    if(new_secret_data.text != "" and new_secret_data.numberOfVisits != 0 and new_secret_data.expDate!=None):
+        return createSecret(new_secret_data, env_variables)
     else: 
         raise Exception("Some data is not provided.")
