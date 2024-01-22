fa50794ece1029884a1e0b5685ad593beca982ff
Tuszmak <daniel.pinter02@gmail.com>
2024-01-22 10:11:41 +0100

    refactor:  make database operations get env variables from server

--- backend/db/getConn.py
+++ backend/db/getConn.py
@@ -1,15 +1,12 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
-
 # This function only exists to reduce code duplication.
-load_dotenv()
 
-def getConn():
+def getConn(envVariables : dict):
     conn = psycopg2.connect(
-              host=os.environ['DB_HOST'],
-              database=os.environ['DB_NAME'],
-              user=os.environ['DB_USERNAME'],
-              password=os.environ['DB_PASSWORD'])
-      
+              host=envVariables.get('DB_HOST'),
+              database=envVariables.get('DB_NAME'),
+              user=envVariables.get('DB_USERNAME'),
+              password=envVariables.get('DB_PASSWORD')
+              )
     return conn
