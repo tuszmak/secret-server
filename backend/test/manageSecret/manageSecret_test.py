52f6ceada0a85ae829dc3219986c91400c2ad2fa
Tuszmak <daniel.pinter02@gmail.com>
2024-01-22 10:43:55 +0100

    refactor: fix patches in test

--- backend/test/manageSecret/manageSecret_test.py
+++ backend/test/manageSecret/manageSecret_test.py
@@ -1,29 +1,42 @@
 from model import SecretData
 from datetime import datetime
 import pytest
-from manageSecret.manageSecret import createSecretDAO
+from service import createSecretService
 from unittest.mock import patch, Mock
 
 class Test_ManageSecret():
     def test_create_secret_without_text(self):
         foo = SecretData("", 5, datetime.now())
         with pytest.raises(Exception):
-            createSecretDAO(foo)
+            createSecretService.createSecretDAO(foo)
     def test_create_secret_without_zero_visits(self):
         foo = SecretData("foo", 0, datetime.now())
         with pytest.raises(Exception):
-            createSecretDAO(foo)
+            createSecretService.createSecretDAO(foo)
     def test_create_secret_without_exp_date(self):
         foo = SecretData("foo", 5, None)
         with pytest.raises(Exception):
-            createSecretDAO(foo)
+            createSecretService.createSecretDAO(foo)
     def test_create_secret_with_no_data(self):
         foo = SecretData("", 0, None)
         with pytest.raises(Exception):
-            createSecretDAO(foo)
-    @patch(target="manageSecret.manageSecret.createSecret")
+            createSecretService.createSecretDAO(foo)
+    @patch(target="service.createSecretService.createSecret")
     def test_creating_data(self, mock_createSecret : Mock):
-        foo = SecretData("foo", 5, datetime.now())
-        createSecretDAO(foo)
+        foo = {
+            "expiryDate": datetime.now(),
+            "secret": "foo",
+            "numberOfVisits": 5
+        }
+        createSecretService.createSecretDAO(foo,None)
         mock_createSecret.assert_called_once()
-        
+    @patch(target="service.createSecretService.createSecret")
+    def test_creating_data_fail(self, mock_createSecret : Mock):
+        foo = {
+            "expiryDate": datetime.now(),
+            "secret": "foo",
+            "numberOfVisits": 5
+        }
+        createSecretService.createSecretDAO(foo,None)
+        mock_createSecret.assert_called_once()
+    
