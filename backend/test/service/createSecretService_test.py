3e68310894d72a5230f8289613472d3982e2542e
Tuszmak <daniel.pinter02@gmail.com>
2024-01-22 12:46:19 +0100

    refactor: refactor createSecret tests to match with refactor

--- backend/test/service/createSecretService_test.py
+++ backend/test/service/createSecretService_test.py
@@ -8,19 +8,19 @@ class Test_CreateSecretService():
     def test_create_secret_without_text(self):
         foo = SecretData("", 5, datetime.now())
         with pytest.raises(Exception):
-            createSecretService.createSecretDAO(foo)
+            createSecretService.create_secret_dao(foo)
     def test_create_secret_without_zero_visits(self):
         foo = SecretData("foo", 0, datetime.now())
         with pytest.raises(Exception):
-            createSecretService.createSecretDAO(foo)
+            createSecretService.create_secret_dao(foo)
     def test_create_secret_without_exp_date(self):
         foo = SecretData("foo", 5, None)
         with pytest.raises(Exception):
-            createSecretService.createSecretDAO(foo)
+            createSecretService.create_secret_dao(foo)
     def test_create_secret_with_no_data(self):
         foo = SecretData("", 0, None)
         with pytest.raises(Exception):
-            createSecretService.createSecretDAO(foo)
+            createSecretService.create_secret_dao(foo)
     @patch(target="service.createSecretService.createSecret")
     def test_creating_data(self, mock_createSecret : Mock):
         foo = {
@@ -28,7 +28,7 @@ class Test_CreateSecretService():
             "secret": "foo",
             "numberOfVisits": 5
         }
-        createSecretService.createSecretDAO(foo,None)
+        createSecretService.create_secret_dao(foo,None)
         mock_createSecret.assert_called_once()
     @patch(target="service.createSecretService.createSecret")
     def test_creating_data_fail(self, mock_createSecret : Mock):
@@ -37,6 +37,6 @@ class Test_CreateSecretService():
             "secret": "foo",
             "numberOfVisits": 5
         }
-        createSecretService.createSecretDAO(foo,None)
+        createSecretService.create_secret_dao(foo,None)
         mock_createSecret.assert_called_once()
     
